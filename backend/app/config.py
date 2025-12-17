057532e46b2f788b296316f74847d546ffb0964d
Mandarin <mandarin@slendbro56.fvds.ru>
2025-12-17 18:49:48 +0300

    Fix: fix env variable name

--- backend/app/config.py
+++ backend/app/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     port: int = 8000
 
     environment: str = os.getenv("ENVIRONMENT", "development")
-    backend_origin: str = os.getenv("BACKEND_ORIGIN_URL", "*")
+    backend_origin_url: str = os.getenv("BACKEND_ORIGIN_URL", "*")
 
     # Paths
     base_dir: Path = Path(__file__).parent.parent.resolve() / "app"
